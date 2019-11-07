43110b22d92d0da5a4dd4da067394c077c0aaf71
hyeonsuk <qu3230@gmail.com>
2019-11-07 19:22:16 +0900

    11.6강 완료

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
@@ -8,7 +8,9 @@ def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.page(int(page))
-    print(vars(rooms.paginator))
-    return render(request, "rooms/home.html", context={"page": rooms},)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", context={"page": rooms},)
+    except EmptyPage:
+        return redirect("/")
 
